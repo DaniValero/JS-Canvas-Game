cf97231c7691cdcbaac9c6ece8f85072ecdd29ea
Antonio Llamas <antonio.llamas@thebridgeschool.es>
2022-10-28 00:36:03 +0200

    subida implementada

--- player.js
+++ player.js
@@ -1,29 +1,35 @@
 class Player {
-    constructor(w, h, ctx, keys) {
+    constructor(w, h, x, y,ctx, keys) {
       this.canvasW = w;
       this.canvasH = h;
       this.ctx = ctx;
       this.keys = keys;
-      this.x = this.canvasW * 0.5;
-      this.y0 = this.canvasH * 0.8
-      this.y = this.y0
 
-      this.vy = 2
+       // Animacion idle
+       this.img = new Image();
+       this.img.src = "images/idle-Sheet.png"
+     
+       this.img.frames = 8;
+       this.img.frameIndex = 0;
+   
+       // medidas de la imagen a representar en el canvas
+       this.w = 50;
+       this.h = 50;
+
+       
+      this.x = x - this.h / 2;
+      
+
+      this.y = y - this.h;
+
+     
+      this.vy = 1
       this.vx = 1
 
       this.dx = 0
-      this.dy = 95
+      this.dy = 1;
 
-      // Animacion idle
-      this.img = new Image();
-      this.img.src = "images/idle-Sheet.png"
-    
-      this.img.frames = 8;
-      this.img.frameIndex = 0;
-  
-      // medidas de la imagen a representar en el canvas
-      this.w = 50;
-      this.h = 50;
+      this.falling = false
 
       this.setListeners()
 
@@ -46,62 +52,53 @@ class Player {
   
     }
   
+    // https://www.w3schools.com/graphics/game_controllers.asp
     setListeners() {
       document.onkeydown = function(event) {
         if (event.keyCode == this.keys.RIGHT_ARROW) {
-        this.dx = 7;
-        this.x += this.dx
-
-        this.img.frames = 8
+        this.dx = 3;
         this.img.src = "images/run-right.png";
-        if (this.img.frameIndex > 7) {this.img.frameIndex = 0}
-        if (this.x < 750) {
-          this.x += 8
-          } else {
-            this.x = 750;
-            this.dx = 0;
-          }
+        this.img.frames = 8
         }
+      
         if (event.keyCode == this.keys.LEFT_ARROW) {
           // this.img.src = "images/jetpack.png"
           // if (this.img.frameIndex > 7) {this.img.frameIndex = 0}
-          this.dx = 7;
-          this.x -= this.dx
+          this.dx = -3;
+          this.img.src = "images/run-left.png";
+          this.img.frames = 8
         } 
       
         // SALTO
 
-        if (event.keyCode == this.keys.SPACE)  {
-          this.dx = 200
+        if (event.keyCode == this.keys.SPACE && !this.falling)  {
           
-          this.y -= this.dx
-          this.vy = 5;
-
+          this.y -= 5
+          this.vy = -10;
+          this.falling = true;
           this.img.src = "images/jetpack.png"
           this.img.frames = 7;
-          if (this.img.frameIndex > 6) this.img.frameIndex = 0;
+          this.img.frameIndex = 0;
         }
       }.bind(this);
 
       document.onkeyup = function(event) {
         if (event.keyCode == this.keys.RIGHT_ARROW) {
-          // this.img.src = "images/idle-Sheet.png"
-          // if (this.img.frameIndex > 7) {this.img.frameIndex = 0}
           this.dx = 0
         }
+
         if (event.keyCode == this.keys.LEFT_ARROW) {
-          // this.img.src = "images/idle-Sheet.png"
-          // if (this.img.frameIndex > 7) {this.img.frameIndex = 0}
-          this.dx = 0  
+          this.dx = 0
         }
+     
 
         if (event.keyCode == this.keys.SPACE) {
 
-          this.img.src = "images/idle-Sheet.png"
-          this.img.frames = 8;
-          if (this.img.frameIndex > 7) this.img.frameIndex = 0;
+          // this.img.src = "images/idle-Sheet.png"
+          // this.img.frames = 8;
+          // if (this.img.frameIndex > 7) this.img.frameIndex = 0;
     
-          this.vy = 1
+          // this.vy = 1
          
         }
       }.bind(this);
@@ -110,27 +107,27 @@ class Player {
 
     move() {
       
-      var gravity = 0.05;
+      var gravity = 0.38;
 
-      if (this.y >= this.y0) {
-        this.vy = 0.75;
-        this.y = this.y0;
+      if (!this.falling) {
+       this.vy = 1;
+        
       } else {
-        this.vy += gravity;
-        this.y += this.vy;
-      }
+         this.vy += gravity;
+         this.y += this.vy;
+       }
 
       // EFECTO ASCENSOR
+
+
       this.y += this.dy
 
-      // MOVER DERECHA
+      // MOVER DER/IZ
       this.x += this.dx
 
-      // MOVER IZQUIERDA
-      this.x -= this.dx
 
       // SALTO
-      this.y -= this.dy
+      // this.y -= this.dy
     }
     
   
@@ -138,7 +135,7 @@ class Player {
   
       if (framesCounter % 5 === 0) {
         this.img.frameIndex += 1;
-        if (this.img.frameIndex > 6) this.img.frameIndex = 0;
+        if (this.img.frameIndex > this.img.frames - 1) this.img.frameIndex = 0;
       }
     }
 
